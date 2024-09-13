81922f9dcb0ab5c5312dbc08041bd0968a874dcd
Fabio Santos <fabio.santos@ifrr.edu.br>
2024-09-13 11:55:30 -0400

    apps, hooks and icon

---
+++ aratinga_seo/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailSeoConfig(AppConfig):
+    name = "aratinga-seo"
+    verbose_name = "Aratinga SEO"
+    default_auto_field = "django.db.models.AutoField"
